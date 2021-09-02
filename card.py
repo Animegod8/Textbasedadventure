73ff34f1e326b2d131683dd36436217937454c85
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-09-02 17:28:40 -0600

    Create card.py

---
+++ card.py
@@ -0,0 +1,15 @@
+print('/n What is your name?')
+name = input()
+print('/n What is your hight')
+hight = input()
+print('/n What kind of mage are you?')
+kind = input()
+
+print('/n so your name is' + name +',' + 'your hight is' + hight +',' 'and you are a' + kind + 'mage.')
+print('/n is this correct y or n.')
+
+awnser = input()
+if awnser == 'y':
+  print('you are redy for your journy my frend')
+elif awnser == 'n':
+  print('fuck you use this command to start over, python3 card.py')
