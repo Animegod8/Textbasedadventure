954e3da4a9b0487807cab9dcf4976a19a4f479ae
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-09-02 18:40:19 -0600

    The choice of adventure

---
+++ chooseAdventure.py
@@ -0,0 +1,17 @@
+def adventureChoose():
+    print('what adventure do you want do go on')
+    print('1, a dagrourus quest to slay a dragon')
+    print('2, a quest for a gem')
+    print('3, TBA(in a future update, more stories will come)')
+    awnser = input()
+    if awnser == '1':
+        print('you chose to go on a slay a dragon')
+
+    elif awnser == '2':
+        print('TBA')
+
+    elif awnser == '3':
+        print('TBA')
+    
+    else:
+        print('not an option. run python3 card.py again.')
