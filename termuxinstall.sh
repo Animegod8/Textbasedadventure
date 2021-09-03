4103797edf5f515af3ba732ca38ff2c812e563ba
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-09-03 14:02:30 -0600

    Termux install scripts

--- termuxinstall.sh
+++ termuxinstall.sh
@@ -3,4 +3,5 @@ pkg upgrade -y
 pkg install python -y
 wget https://github.com/Animegod8/Textbasedadventure/archive/refs/heads/main.zip
 unzip Textbasedadventure-main.zip
+cd Textbasedadventure
 python3 card.py
