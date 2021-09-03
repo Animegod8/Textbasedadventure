5f2fd272307bf768c3892b8d1be8b084b17942b3
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-09-03 14:05:16 -0600

    Debian install script

--- debianinstall.sh
+++ debianinstall.sh
@@ -4,5 +4,5 @@ apt upgrade -y
 apt install python
 wget https://github.com/Animegod8/Textbasedadventure/archive/refs/heads/main.zip
 unzip Textbasedadventure-main.zip
-cd Textbasedadventure
+cd Textbasedadventure-main
 python3 card.py
