7b15f2ef26878052912c48997158a606fc05e5cd
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-09-03 13:51:30 -0600

    Ubuntu install file

---
+++ ubuntuinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo pat-get upgrade -y
+sudo apt install python
+wget https://github.com/Animegod8/Textbasedadventure/archive/refs/heads/main.zip
+unzip Textbasedadventure-main.zip
+python3 card.py
