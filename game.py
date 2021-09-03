06d3ae0e20bf502099edf08beb8bd48403bfec82
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-09-03 08:20:51 -0600

    Add files via upload

---
+++ game.py
@@ -0,0 +1,33 @@
+def adven1():
+    print('you start at a tavern and you start your journy.')
+    print('and you are at a cross section.')
+    print('do you 1, take the path of peril, or 2 take the express way')
+    awnser input()
+    if awnser == '1':
+        print('you take the path of peril')
+        pathoperil()
+
+    elif awnser = '2':
+        print('well you take the express route')
+        
+    def pathoperil():
+    print('Hello world!')
+    
+def adventureChoose():
+    print('what adventure do you want do go on')
+    print('1, a dagrourus quest to slay a dragon')
+    print('2, a quest for a gem')
+    print('3, TBA(in a future update, more stories will come)')
+    awnser = input()
+    if awnser == '1':
+        print('you chose to go on a slay a dragon')
+        adven1()
+
+    elif awnser == '2':
+        print('TBA')
+
+    elif awnser == '3':
+        print('TBA')
+    
+    else:
+        print('not an option. run python3 card.py again.')
