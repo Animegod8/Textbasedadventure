0bc857cdf76a7601284e8fbf473e20f9e8d80c5c
Animegod8 <60558787+Animegod8@users.noreply.github.com>
2021-09-03 14:06:21 -0600

    Ubuntu install script

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -4,5 +4,5 @@ sudo pat-get upgrade -y
 sudo apt install python
 wget https://github.com/Animegod8/Textbasedadventure/archive/refs/heads/main.zip
 unzip Textbasedadventure-main.zip
-cd Textbasedadventure
+cd Textbasedadventure-main
 python3 card.py
